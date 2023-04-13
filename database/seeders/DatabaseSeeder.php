a79acecf89f3e490b0d3f5f4173ad64e3bc3f5fa
yamz-11 <wiratamajrz@gmail.com>
2023-04-13 22:34:54 +0700

    Create, Read, Search Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,17 @@ class DatabaseSeeder extends Seeder
                 'is_admin' => true
             ]
         );
+        User::create(
+            [
+                'name' => 'Wiratama',
+                'email' => 'Wiratamajrz@gmail.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
+                'remember_token' => Str::random(10),
+                'is_admin' => false
+            ]
+        );
+
         User::factory(100)->create();
         Todo::factory(500)->create();
     }
